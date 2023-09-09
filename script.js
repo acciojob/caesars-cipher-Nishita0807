9799de673e1946411a4c5706d6c7f76df39b0ddf
Nishita Rani <92663360+Nishita0807@users.noreply.github.com>
2023-09-09 13:54:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -37,40 +37,7 @@ function rot13(encodedStr) {
 
   // Your Script here.
 
-const lookup = {
-  A: "N",
-  B: "O",
-  C: "P",
-  D: "Q",
-  E: "R",
-  F: "S",
-  G: "T",
-  H: "U",
-  I: "V",
-  J: "W",
-  K: "X",
-  L: "Y",
-  M: "Z",
-  N: "A",
-  O: "B",
-  P: "C",
-  Q: "D",
-  R: "E",
-  S: "F",
-  T: "G",
-  U: "H",
-  V: "I",
-  W: "J",
-  X: "K",
-  Y: "L",
-  Z: "M",
-  "?": "?",
-  ",": ",",
-};
 
-function rot13(encodedStr) {
-  let decodedArr = []; // Your Result goes here
-  // Only change code below this line
 
 	 for (let i = 0; i < encodedStr.length; i++) {
     const char = encodedStr[i];
@@ -91,14 +58,12 @@ function rot13(encodedStr) {
 
 //console.log(rot13("SERR YBIR? NPPVBWBO"));
 
-// Do not change this line
-window.rot13 = rot13;
 
-}
+
 
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
-// console.log(rot13("SERR YBIR? NPPVBWBO"));
+ console.log(rot13("SERR YBIR? NPPVBWBO"));
 
 // Do not change this line
 window.rot13 = rot13;
